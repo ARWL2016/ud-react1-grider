738ef3b03147b6c43d448b05e8b11cd5754381ad
Alistair Willis <alistairrwillis@gmail.com>
2017-03-06 20:37:57 +0900

    add API key

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'; 
+import SearchBar from './components/search_bar';
+
+const API_KEY = AIzaSyDlDJRFWCeRY-bC3qRaXEdysQM0-NfcvxU;  
 
 const App = () => {
   return (
