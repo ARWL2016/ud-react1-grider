7d8ea9e07b3341e33b99658920f087db3a96889b
Alistair Willis <alistairrwillis@gmail.com>
2017-04-01 15:44:33 +0900

    bug fix

--- BlogApp/src/actions/index.js
+++ BlogApp/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'; 
 
 export const FETCH_POSTS = 'FETCH_POSTS'; 
+export const CREATE_POST = 'CREATE_POSTS'; 
+// a variable is easier to debug than a random string
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=hsaidfs' ; 
@@ -12,4 +14,13 @@ export function fetchPosts() {
         type: FETCH_POSTS, 
         payload: request
     };
+}
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
 }
--- BlogApp/src/components/posts_new.js
+++ BlogApp/src/components/posts_new.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { reduxForm }  from 'redux-form'; 
+import { createPost } from '../actions/index'; 
 
 class PostsNew extends Component {
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate the user to the index
+        // We navigate by calling this.context.router.push with the
+        // new path to navigate to.
+        this.context.router.push('/');
+      });
+  }
   render() {
     const { fields: {title, categories, content}, handleSubmit } = this.props; // from redux-form
     //  same as: const title = this.props.fields.title; 
     return(
-        <form onSubmit={handleSubmit}> 
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}> 
           <h3>Create a New Post</h3>
           <div className="form-group">
             <label>Title</label>
@@ -30,16 +40,20 @@ class PostsNew extends Component {
 }
 
 export default reduxForm({
+  form: 'PostsNew', 
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew); 
+
+
+
   /* 
   configure redux-form with field names 
   this enables redux-form to capture input as global application state
   using form reducer 
   redux-form is a wrapper for the PostsNew component
   Like connect, it is injecting helpers into our props
+  The arguments are the same as connect, but there is an extra
+  first argument: this configuration object 
   */
-  form: 'PostsNewForm', 
-  fields: ['title', 'categories', 'content']
-
-})(PostsNew); 
 
 // object destructuring in line 13 adds methods like onChange, onBlur to our form input 
